68d4add6919ac2e00ef1e0b6b08fc9a6f57a5080
Jakub Wroński <j@wronski.cloud>
2025-08-23 18:34:36 +0200

    feat(user-service): add tests for initial implementation

--- user-service/domain/src/main/java/com/github/bovvver/vo/Email.java
+++ user-service/domain/src/main/java/com/github/bovvver/vo/Email.java
@@ -4,9 +4,13 @@ import java.util.regex.Pattern;
 
 public record Email(String value) {
 
-    private static final Pattern EMAIL_REGEX = Pattern.compile("^((?!\\.)[\\w-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$");
+    private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
 
     public Email {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be blank");
+        }
+
         if (!EMAIL_REGEX.matcher(value).matches()) {
             throw new IllegalArgumentException("Invalid email format: " + value);
         }
