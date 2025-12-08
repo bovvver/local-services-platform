814b5ce31e438727957cb8abcb1cd111322b92b3
Jakub Wroński <j@wronski.cloud>
2025-12-08 19:18:29 +0100

    fix(salary-type): unify salary type throughout the application v2

--- booking-service/src/main/java/com/github/bovvver/bookingmanagement/bookingdraftcreation/BookOfferRequest.java
+++ booking-service/src/main/java/com/github/bovvver/bookingmanagement/bookingdraftcreation/BookOfferRequest.java
@@ -1,11 +1,11 @@
 package com.github.bovvver.bookingmanagement.bookingdraftcreation;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public record BookOfferRequest(@NotNull UUID offerId,
-                               @PositiveOrZero BigDecimal salary) {
+                               @Positive BigDecimal salary) {
 }
