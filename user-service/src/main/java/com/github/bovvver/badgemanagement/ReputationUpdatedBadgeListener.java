9b1c5d37ff81fc5173ff1ff9f3bef9fd8582c9c9
Jakub Wroński <j@wronski.cloud>
2026-08-15 19:44:18 +0200

    refactor(listener): replace transactional event listeners with standard event listeners for user creation events

--- user-service/src/main/java/com/github/bovvver/badgemanagement/ReputationUpdatedBadgeListener.java
+++ user-service/src/main/java/com/github/bovvver/badgemanagement/ReputationUpdatedBadgeListener.java
@@ -3,11 +3,8 @@ package com.github.bovvver.badgemanagement;
 import com.github.bovvver.reputationmanagement.ReputationUpdated;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -16,8 +13,7 @@ class ReputationUpdatedBadgeListener {
 
     private final BadgeAssignmentService badgeAssignmentService;
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     public void handle(ReputationUpdated event) {
         badgeAssignmentService.addBadges(event);
     }
